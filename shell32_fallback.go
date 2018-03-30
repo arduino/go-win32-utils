02c0d98063446bdfbae95b3e54e9b6462bce8289
Cristian Maglie <c.maglie@arduino.cc>
2018-03-30 21:37:22 +0200

    Added further comments

--- shell32_fallback.go
+++ shell32_fallback.go
@@ -36,14 +36,20 @@ import (
 	"runtime"
 )
 
+// The functions defined below allow compile on non-Windows OS. The caller
+// may choose to not call those functions based on runtime.GOOS value.
+
+// GetDocumentsFolder returns the Document folder
 func GetDocumentsFolder() (string, error) {
 	return "", fmt.Errorf("operating system not supported: %s", runtime.GOOS)
 }
 
+// GetLocalAppDataFolder returns the LocalAppData folder
 func GetLocalAppDataFolder() (string, error) {
 	return "", fmt.Errorf("operating system not supported: %s", runtime.GOOS)
 }
 
+// GetRoamingAppDataFolder returns the AppData folder
 func GetRoamingAppDataFolder() (string, error) {
 	return "", fmt.Errorf("operating system not supported: %s", runtime.GOOS)
 }
