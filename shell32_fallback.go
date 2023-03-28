7d852ce37336297c1231274227084e8a57b45fef
MatteoPologruto <m.pologruto@ext.arduino.cc>
2023-03-28 13:11:07 +0200

    Add license header to project's files

--- shell32_fallback.go
+++ shell32_fallback.go
@@ -1,5 +1,11 @@
 //go:build !windows
 
+//
+// Copyright 2018-2023 ARDUINO SA. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+
 package win32
 
 import (
