1ff2441f50924c52563bf4500c483ff9839bc6d8
MatteoPologruto <m.pologruto@ext.arduino.cc>
2023-03-28 13:18:59 +0200

    Revert removal of win32 package's description

--- doc.go
+++ doc.go
@@ -4,4 +4,6 @@
 // license that can be found in the LICENSE file.
 //
 
+// Package win32 is a collection of useful bindings to Win32 API that are not available in the standard
+// golang windows/syscall package.
 package win32
